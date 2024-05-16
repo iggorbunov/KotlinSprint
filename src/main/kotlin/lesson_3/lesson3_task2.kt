8bba062c84d2f3961707e503531d2e9bae67928b
iggorbunov <igorbunov2001@yandex.ru>
2024-05-17 01:52:15 +0300

    KS-3-2 fullname to different years

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main() {
+    val firstName = "Татьяна"
+    var secondName = "Андреева"
+    val surname = "Сергеевнa"
+
+    var years = 20
+
+    println("$secondName $firstName $surname, $years лет")
+
+    secondName = "Сидорова"
+    years = 22
+
+    println("$secondName $firstName $surname, $years года")
+}
