a102ce9018297d1a2dc0085edc2073edaf1a8c25
iggorbunov <igorbunov2001@yandex.ru>
2024-05-16 18:18:29 +0300

    Revert "KS-1-5 average grade student"
    This reverts commit 6ed655d7fabace10a0f7b1145b2b4051617a6cd9.

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,15 +2,17 @@ package org.example.lesson_1
 
 fun main() {
     val year = 1961
-    var hour: String = "09"
-    var minute: String = "07"
+    var hour: Int = 9
+    var minute: Int = 7
 
     println(year)
     println(hour)
     println(minute)
 
-    hour = "10"
-    minute = "55"
+    hour = 10
+    minute = 55
 
-    println("$hour:$minute")
+    print(hour)
+    print(":")
+    print(minute)
 }
