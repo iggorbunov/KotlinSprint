2580232d3d3b46ffd3266daac762f600a8bed37e
iggorbunov <igorbunov2001@yandex.ru>
2024-05-17 19:04:48 +0300

    KS-2-4 fix percent variable

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,9 +3,10 @@ package org.example.lesson_2
 fun main() {
     val crystalsOre = 7
     val ironOre = 11
+    var buffPercent = 0.2
 
-    val crystalOreFromBuff = crystalsOre * 0.2
-    val ironOreFromBuff = ironOre * 0.2
+    val crystalOreFromBuff = crystalsOre * buffPercent
+    val ironOreFromBuff = ironOre * buffPercent
 
     print("""
         ${crystalOreFromBuff.toInt()} Кристалической руды
