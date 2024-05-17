c0828e51dc6884a0347c03b4bb33faf370541dc3
iggorbunov <igorbunov2001@yandex.ru>
2024-05-17 19:12:17 +0300

    KS-2-5 fix interest input

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -3,10 +3,11 @@ import kotlin.math.pow
 
 fun main() {
     val initialContribution = 70000
-    val interestRate = 0.167
+    var interestRate = 16.7
+    interestRate = interestRate / 100 // Не понимаю как лучше сделать, что бы не было магических чисел, но и не заводить const для 100
     val yearCount = 20
 
-    val finalContribution: Double = initialContribution * (1 + interestRate).pow(yearCount)
+    val finalContribution: Double = initialContribution * ((1 + (interestRate))).pow(yearCount)
 
     println("%.3f".format(finalContribution))
 }
