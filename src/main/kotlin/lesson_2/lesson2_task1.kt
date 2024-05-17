618c2831d3ce773c8f8de1fcf0e236ca7c1914f2
iggorbunov <igorbunov2001@yandex.ru>
2024-05-17 13:16:13 +0300

    KS-2-1 fix student number

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,7 +1,7 @@
 package org.example.lesson_2
 
 fun main() {
-    val averageRating: Float = (3 + 4 + 3 + 5) / 4.00f
+    val averageRating: Float = (3.00f + 4.00f + 3.00f + 5.00f) / 4
 
     println(averageRating)
 }
