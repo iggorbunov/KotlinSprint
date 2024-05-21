a8650e36097e60a4b822d7c8fa94bf38fd3e538f
iggorbunov <igorbunov2001@yandex.ru>
2024-05-21 12:43:42 +0300

    KS-4-4 fix boolean variable

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,13 +2,14 @@ package org.example.lesson_4
 
 fun main() {
     val currentTrainingDay = 4
-    val armAndAbdominal = currentTrainingDay % 2 != 0
-    val legAndBack = currentTrainingDay % 2 == 0
+    val isEvenDay = currentTrainingDay % 2
+    val isArmAndAbdominalDay = isEvenDay != 0
+    val isLegAndBackDay = isEvenDay == 0
 
     println("""
-        Упражнения для рук:    $armAndAbdominal
-        Упражнения для ног:    $legAndBack
-        Упражнения для спины:  $legAndBack
-        Упражнения для пресса: $armAndAbdominal
+        Упражнения для рук:    $isArmAndAbdominalDay
+        Упражнения для ног:    $isLegAndBackDay
+        Упражнения для спины:  $isLegAndBackDay
+        Упражнения для пресса: $isArmAndAbdominalDay
     """.trimIndent())
 }
