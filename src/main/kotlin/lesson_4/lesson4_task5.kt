881c0cdc5ecaf7fc1717a59ef329125c66b048b9
iggorbunov <igorbunov2001@yandex.ru>
2024-05-21 12:47:53 +0300

    KS-4-5 fix input variable and print understood text

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -8,19 +8,19 @@ const val GOOD_WEATHER = true
 
 fun main() {
     print("Есть повреждения корабля: ")
-    val damage = readlnOrNull().toBoolean()
+    val damage = readLine().toBoolean()
 
     print("Число экипажа: ")
-    val crew = readlnOrNull()?.toInt()
+    val crew = readLine()!!.toInt()
 
     print("Количество ящиков с провизией: ")
-    val boxesProvisions = readLine()?.toInt()
+    val boxesProvisions = readLine()!!.toInt()
 
     print("Благоприятная ли погода: ")
     val weather = readLine().toBoolean()
 
-    val departureShip = (DAMAGE == damage && (FROM_CREW <= crew!! && crew <= TO_CREW) && MIN_BOXES_PROVISIONS < boxesProvisions!!)
-            || (TO_CREW == crew && GOOD_WEATHER == weather && MIN_BOXES_PROVISIONS <= boxesProvisions!!)
+    val departureShip = (DAMAGE == damage && (FROM_CREW <= crew && crew <= TO_CREW) && MIN_BOXES_PROVISIONS < boxesProvisions)
+            || (TO_CREW == crew && GOOD_WEATHER == weather && MIN_BOXES_PROVISIONS <= boxesProvisions)
 
-    println(departureShip)
+    println("Корабль может выходить в плавание: $departureShip")
 }
