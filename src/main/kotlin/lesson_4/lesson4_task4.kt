fbd02f0628b1518740f1319e99cb1f5d37ec8252
iggorbunov <igorbunov2001@yandex.ru>
2024-05-23 00:02:53 +0300

    KS-4-4 fix use one variable

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,14 +2,13 @@ package org.example.lesson_4
 
 fun main() {
     val currentTrainingDay = 4
-    val isEvenDay = currentTrainingDay % 2
-    val isArmAndAbdominalDay = isEvenDay != 0
-    val isLegAndBackDay = isEvenDay == 0
+    val isEvenDay = currentTrainingDay % 2 == 0
+
 
     println("""
-        Упражнения для рук:    $isArmAndAbdominalDay
-        Упражнения для ног:    $isLegAndBackDay
-        Упражнения для спины:  $isLegAndBackDay
-        Упражнения для пресса: $isArmAndAbdominalDay
+        Упражнения для рук:    ${!isEvenDay}
+        Упражнения для ног:    ${isEvenDay}
+        Упражнения для спины:  ${isEvenDay}
+        Упражнения для пресса: ${!isEvenDay}
     """.trimIndent())
 }
