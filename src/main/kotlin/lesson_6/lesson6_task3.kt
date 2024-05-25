0bcae82ec6e6b55cc787f3759bd74c6ee11d5fd6
iggorbunov <igorbunov2001@yandex.ru>
2024-05-26 01:18:32 +0300

    KS-6-3 fix, use one variable

--- src/main/kotlin/lesson_6/lesson6_task3.kt
+++ src/main/kotlin/lesson_6/lesson6_task3.kt
@@ -3,12 +3,9 @@ package org.example.lesson_6
 fun main() {
     println("Ввведите количество секунд, которое хотите засечь на таймере:")
     var timerCount = readln().toInt()
-    var secondsCount = 1
     while (timerCount > 0) {
+        println("Осталось секунд: ${timerCount--}")
         Thread.sleep(1000)
-        println("Прошло $secondsCount секунд")
-        secondsCount++
-        timerCount--
     }
     println("Время вышло")
 }
