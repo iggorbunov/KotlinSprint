c0b08b48b9ccb10675986c6daf5353383e4fc9d2
iggorbunov <igorbunov2001@yandex.ru>
2024-05-26 00:40:09 +0300

    KS-6-2 fix, one use .sleep

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -2,11 +2,7 @@ package org.example.lesson_6
 
 fun main() {
     println("Ввведите количество секунд, которое хотите засечь на таймере:")
-    var timerCount = readln().toInt()
-    val secondsCount = timerCount
-    while (timerCount > 0) {
-        Thread.sleep(1000)
-        timerCount--
-    }
-    println("Прошло $secondsCount секунд")
+    val timerCount = readln().toLong()
+    Thread.sleep(1000 * timerCount)
+    println("Прошло $timerCount секунд")
 }
