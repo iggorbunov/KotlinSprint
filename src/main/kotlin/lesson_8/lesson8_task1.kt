1b56126cdcfc45961c4325910b3427c2f2a38b56
iggorbunov <igorbunov2001@yandex.ru>
2024-05-28 19:28:15 +0300

    KS-8-1 fix array sum and clear code

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -3,8 +3,6 @@ package org.example.lesson_8
 import kotlin.random.Random
 
 fun main() {
-    val views = IntArray(7, { Random.nextInt(1000, 10000) }) //{ Random.nextInt(1000, 10000) }
-    var sumViews = 0
-    for (i in views) sumViews += i
-    println("Суммарное количество просмотров: $sumViews")
+    val views = IntArray(7, { Random.nextInt(1000, 10000) })
+    println("Суммарное количество просмотров: ${views.sum()}")
 }
