9c7eb63103aea7b7cf418cf7a5bf3d0f6f51bc66
iggorbunov <igorbunov2001@yandex.ru>
2024-05-30 02:02:48 +0300

    KS-9-1 output list

---
+++ src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_9
+
+fun main() {
+    val recipeIngredient = listOf(
+        "Куриный бульон", "Паста том-ям", "Лимон", "Рыбный соус", "Лимонная трава", "Имбирь",
+        "Креветки", "Шампиньоны", "Кокосовое молоко", "Кинза"
+    )
+    println("В рецепте есть следующие ингредиенты:")
+    recipeIngredient.forEach { println(it) }
+}
