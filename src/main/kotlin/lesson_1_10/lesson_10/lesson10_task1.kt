499a6da395f7b624e8c44c67b1d77418793b90d8
iggorbunov <igorbunov2001@yandex.ru>
2024-05-31 01:51:47 +0300

    KS-10-1 dice game

---
+++ src/main/kotlin/lesson_1_10/lesson_10/lesson10_task1.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1_10.lesson_10
+
+import kotlin.random.Random
+
+fun main() {
+    val resultUser = diceThrow()
+    val resultComputer = diceThrow()
+    println("Ход игрока: $resultUser\nХод компьютера: $resultComputer")
+    if (resultUser > resultComputer) println("Победило человечество")
+    else if (resultUser < resultComputer) println("Победила машина")
+    else println("Ничья")
+}
+
+fun diceThrow(): Int {
+    return Random.nextInt(1, 6)
+}
