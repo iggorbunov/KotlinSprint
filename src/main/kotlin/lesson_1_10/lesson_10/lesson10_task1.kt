8bc04e6ef727a8ae9469d454896754edd4bc64b7
iggorbunov <igorbunov2001@yandex.ru>
2024-05-31 11:26:02 +0300

    KS-10-1 fix, use function .random and use when

--- src/main/kotlin/lesson_1_10/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_1_10/lesson_10/lesson10_task1.kt
@@ -1,16 +1,17 @@
 package org.example.lesson_1_10.lesson_10
 
-import kotlin.random.Random
-
 fun main() {
     val resultUser = diceThrow()
     val resultComputer = diceThrow()
     println("Ход игрока: $resultUser\nХод компьютера: $resultComputer")
-    if (resultUser > resultComputer) println("Победило человечество")
-    else if (resultUser < resultComputer) println("Победила машина")
-    else println("Ничья")
+    if (resultUser == resultComputer) {
+        println("Ничья")
+    } else when (resultUser > resultComputer) {
+        true -> println("Победило человечество")
+        false -> println("Победила машина")
+    }
 }
 
 fun diceThrow(): Int {
-    return Random.nextInt(1, 6)
+    return (1..6).random()
 }
