d84a7088d290d5d7c5447b64ef07b6de6587e23e
iggorbunov <igorbunov2001@yandex.ru>
2024-05-31 11:01:34 +0300

    KS-9-5 fix, use set instead list

--- src/main/kotlin/lesson_1_10/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_1_10/lesson_9/lesson9_task5.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_1_10.lesson_9
 
 fun main() {
-    var recipeList = mutableListOf<String>()
+    var recipeList = mutableSetOf<String>()
     println("Введите 5 ингредиентов")
     for (i in 1..5) recipeList.add(readln())
-    recipeList = recipeList.sorted().distinct().toMutableList()
+    recipeList = recipeList.sorted().toMutableSet()
     recipeList.forEach {
         when (recipeList.indexOf(it)) {
             0 -> print("${it.capitalize()}, ")
