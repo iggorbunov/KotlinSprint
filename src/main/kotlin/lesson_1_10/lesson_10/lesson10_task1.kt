0013e39a09e6fe9caff5be7692e090779644d090
iggorbunov <igorbunov2001@yandex.ru>
2024-06-01 10:29:26 +0300

    KS-10-1 fix use only when

--- src/main/kotlin/lesson_1_10/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_1_10/lesson_10/lesson10_task1.kt
@@ -1,17 +1,16 @@
 package org.example.lesson_1_10.lesson_10
 
-import kotlin.random.Random
-
 fun main() {
     val resultUser = diceThrow()
     val resultComputer = diceThrow()
     println("Ход игрока: $resultUser\nХод компьютера: $resultComputer")
-    if (resultUser > resultComputer) println("Победило человечество")
-    else if (resultUser < resultComputer) println("Победила машина")
-    else println("Ничья")
+    when {
+        (resultUser > resultComputer) -> println("Победило человечество")
+        (resultUser < resultComputer) -> println("Победила машина")
+        (resultUser == resultComputer) -> println("Ничья")
+    }
 }
 
-
 fun diceThrow(): Int {
-    return Random.nextInt(1, 6)
+    return (1..6).random()
 }
